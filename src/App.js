08d886cb8bd7e23515cde86e686f94b0727bca2b
Mortron <cmsl4y3r@gmail.com>
2021-11-14 13:09:40 -0700

    807 pokemon displayed per the assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+    .then((res) => {
+      console.log(res)
+      return res.json();
+    })
+    .then((res) =>{
+      console.log(res)
+      setPokemon(res.results);
+    })
+    .catch((err) =>console.log(err))
+  }, []);
+
   return (
     <div className="App">
-      
+     {console.log(pokemon)}
+      {
+      pokemon.map((poke, index)=>{
+        return(
+          <div key={index}>
+            {poke.name}
+          </div>
+        )
+      })}
     </div>
-    
-  );
+  )
 }
 
 export default App;
