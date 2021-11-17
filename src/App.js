4612b592eaeec85a062e83366956e6c33c3aeed2
Mortron <cmsl4y3r@gmail.com>
2021-11-16 20:04:50 -0700

    added some cool interactive stuff to fetch.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
-import PokeAxios from './components/PokeAxios';
-// import PokeFetch from './components/PokeFetch';
+// import PokeAxios from './components/PokeAxios';
+import PokeFetch from './components/PokeFetch';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
 
   return (
     <>
-      {/* <PokeFetch pokemon={pokemon} setPokemon={setPokemon}/> */}
-      <PokeAxios 
+      <PokeFetch pokemon={pokemon} setPokemon={setPokemon}/>
+      {/* <PokeAxios 
       pokemon={pokemon} 
       setPokemon={setPokemon}
       
-      />
+      /> */}
     </>
   )
 }
--- src/components/PokeFetch.js
+++ src/components/PokeFetch.js
@@ -1,12 +1,14 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PokeFetch = (props) => {
     
     const {pokemon, setPokemon} = props;
-    
+    const [x, setX] = useState(1);
     useEffect(() => {
-        // there's actually 1118 pokemon but the assignment says 807
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+        // there's actually 1118 pokemon but the assignment says 807.  I ended 
+        // making the x value the limit.  Everytime you click the button you get 1
+        // more.  Gotta click 'em all!!! make sure to include the $
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${x}`)
         .then((res) => {
             console.log(res)
             return res.json();
@@ -18,20 +20,29 @@ const PokeFetch = (props) => {
             setPokemon(res.results);
         })
         .catch((err) =>console.log(err))
-    }, []);
+        // by putting x inside the array the item rerenders each time the button is 
+        // clicked
+    }, [x]);
     
     return(
 
         <div>
+            {/* learned a cool way to get the items to rerender! */}
+        <div style={{textAlign:"center"}}>
+            <button  onClick={() => setX(x+50)}>Rerender Component</button>
+        </div>
             {console.log(pokemon)}
             {
             pokemon.map((poke, index)=>{
             return(
                 <div key={index} style={{textAlign:"center"}}>
                     {poke.name}
+                    
                 </div>
+                
             )
         })}
+        
         </div>
     )
 }
